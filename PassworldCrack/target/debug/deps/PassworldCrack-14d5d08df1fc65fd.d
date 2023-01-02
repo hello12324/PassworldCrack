e712637de56ae859be1b5b82a5ea0dc744274220
geumbo <geumbo@pop-os.localdomain>
2023-01-03 00:51:50 +0900

    test project 2023-1-3

---
+++ PassworldCrack/target/debug/deps/PassworldCrack-14d5d08df1fc65fd.d
@@ -0,0 +1,5 @@
+/home/geumbo/Documents/MyProgramProject/PassworldCrack/target/debug/deps/PassworldCrack-14d5d08df1fc65fd: src/main.rs
+
+/home/geumbo/Documents/MyProgramProject/PassworldCrack/target/debug/deps/PassworldCrack-14d5d08df1fc65fd.d: src/main.rs
+
+src/main.rs:
